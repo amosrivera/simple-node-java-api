33a36d669b93d7bc3665eee38055e8c97246b1e1
Amós Rivera <arivera.utpmp@gmail.com>
2014-04-11 11:58:33 -0600

    removing modules from repository. adding java module dependency

--- routes/rest.js
+++ routes/rest.js
@@ -4,7 +4,7 @@ exports.test = function (req,res) {
 
 exports.helloWorld = function (req,res) {
 	var exec = require('child_process').exec, child;
-	
+
 	child = exec('/usr/bin/java -jar '+__dirname+'/../ws.jar', function (error, stdout, stderr) {
 		console.log('stdout: ' + stdout);
 		console.log('stderr: ' + stderr);
@@ -13,4 +13,13 @@ exports.helloWorld = function (req,res) {
 		}
 		process.exit();
 	});
+}
+
+exports.javaPackageExample = function(req,res) {
+	var java 	= require('java');
+	java.classpath.push('/sites/simple-node-java-api/example1.jar');
+	java.callStaticMethod('com.ser.as400.webservice.Lubricantes','name','test',function(err,results){
+		console.log(err,results);
+		process.exit();
+	})
 }
